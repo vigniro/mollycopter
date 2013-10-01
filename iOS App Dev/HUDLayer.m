a2bb2fccc07543e6f92aef7c17f7119d94ca4170
Vignir Gudmundsson <vignirgudmunds@gmail.com>
2013-10-01 13:07:28 +0000

    Credits given to tutorials by Ray Wenderlich and SUPERSURACOON

--- iOS App Dev/HUDLayer.m
+++ iOS App Dev/HUDLayer.m
@@ -5,6 +5,7 @@
 //  Created by Solveig Sif Gudmundsdottir on 9/30/13.
 //  Copyright (c) 2013 Sveinn Fannar Kristjansson. All rights reserved.
 //
+//  Credits to Ray Wenderlich tutorial
 
 #import "HUDLayer.h"
 #import "Game.h"
